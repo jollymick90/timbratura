99d553838736c7b5e56c5c410dce4723703ff7d9
Michele Scarpa <michele.scarpa@Vemsolutions.it>
2021-09-30 18:49:08 +0200

    create timework input hours minutes component

---
+++ src/components/TimeWorkInput.tsx
@@ -0,0 +1,15 @@
+import { hoursMinutes } from '../core/time-print';
+import { extractTimeWorkHM } from '../core/timer-utils';
+
+const TimeWorkInput = () => {
+
+    
+    return (
+        <>
+        
+        </>
+    )
+
+}
+
+export default TimeWorkInput;
